027c3d80cbeb5d3b9bca353bcbfb10b286c90391
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2023-02-02 15:34:13 +0000

    basic organize method

--- src/org/emeraldcraft/apcsa/poker/Card.java
+++ src/org/emeraldcraft/apcsa/poker/Card.java
@@ -1,28 +1,55 @@
 package org.emeraldcraft.apcsa.poker;
 
+import static org.emeraldcraft.apcsa.poker.Card.Constants.CARD_VALUES;
+
+import java.util.HashMap;
+
 // Card class represents a playing card
 
-public class Card
-{
-	
-	private String face; // face of card ie..."Ace","Deuce", "King"..etc
-	private String suit; // suit of card ie "Heart", "Diamond"
-	
+public class Card {
+	public static class Constants {
+		public static final HashMap<String, Integer> CARD_VALUES = new HashMap<>();
+		static {
+			// lazy way out
+			CARD_VALUES.put("Deuce", 2);
+			CARD_VALUES.put("Three", 3);
+			CARD_VALUES.put("Four", 4);
+			CARD_VALUES.put("Five", 5);
+			CARD_VALUES.put("Six", 6);
+			CARD_VALUES.put("Seven", 7);
+			CARD_VALUES.put("Eight", 8);
+			CARD_VALUES.put("Nine", 9);
+			CARD_VALUES.put("Ten", 10);
+			CARD_VALUES.put("Jack", 11);
+			CARD_VALUES.put("Queen", 12);
+			CARD_VALUES.put("King", 13);
+			CARD_VALUES.put("Ace", 14);
+		}
+	}
+
+	private final String face; // face of card ie..."Ace","Deuce", "King"..etc
+	private final String suit; // suit of card ie "Heart", "Diamond"
+	private final int value;
+
+	public int getValue() {
+		return value;
+	}
+
 	// two argument card constructor
-	public Card( String cardFace, String cardSuit )
-	{
+	public Card(String cardFace, String cardSuit) {
 		face = cardFace;
 		suit = cardSuit;
+		// Convert it to a value
+		value = CARD_VALUES.get(cardFace);
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return face + " of " + getUnicode();
 	}
-	private String getUnicode()
-	{
-		switch(suit) {
-			case "Hearts": 
+
+	private String getUnicode() {
+		switch (suit) {
+			case "Hearts":
 				return "\u2665";
 			case "Diamonds":
 				return "\u25C6";
@@ -34,5 +61,5 @@ public class Card
 				throw new IllegalArgumentException("Invalid suit of " + suit + "!");
 		}
 	}
-	
+
 }
--- src/org/emeraldcraft/apcsa/poker/Poker.java
+++ src/org/emeraldcraft/apcsa/poker/Poker.java
@@ -1,7 +1,9 @@
 package org.emeraldcraft.apcsa.poker;
 
+import java.util.Arrays;
+
 public class Poker {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         DeckOfCards cards = new DeckOfCards();
         Card[] player1 = new Card[5];
         Card[] player2 = new Card[5];
@@ -13,15 +15,36 @@ public class Poker {
         System.out.println("Player 1 Cards:");
         for (int i = 0; i < player1.length; i++) {
             player1[i] = cards.dealCard();
-            System.out.println("Card " + (i + 1) + ":  " + player1[i]);
         }
-        System.out.println("\n\nPlayer 2 Cards: ");
         for (int i = 0; i < player2.length; i++) {
             player2[i] = cards.dealCard();
-            System.out.println("Card " + (i + 1) + ":  " + player2[i]);
+        }
+        //Organize our Decks
+        organizeDeck(player1);
+        organizeDeck(player2);
+        System.out.println(Arrays.toString(player1));
+        System.out.println(Arrays.toString(player2));
+    }
+    public static void organizeDeck(Card[] hand){
+        Card temp;
+        for(int i = 0; i < hand.length; i++){
+            int minIndex = findMinimum(hand, i);
+            if(minIndex != i){
+                //swap the cards
+                temp = hand[i];
+                hand[i] = hand[minIndex];
+                hand[minIndex] = temp;
+            }
         }
     }
-    public static void loadingAnimation(String during, String end, double duration) throws InterruptedException{
+    private static int findMinimum(Card[] hand, int first) {
+        int minIndex = first;
+        for(int i = first + 1; i < hand.length; i++){
+            if(hand[i].getValue() < hand[minIndex].getValue()) minIndex = i;
+        }
+        return minIndex;
+    }
+    public static void loadingAnimation(String during, String end, double duration) {
         int amount = (int) (duration/0.5);
         for(int i = 0; i < amount; i++){
             String text = during;
@@ -31,7 +54,12 @@ public class Poker {
             }
             //Input whitespace to be able to override previous
             text += "   ";
-            Thread.sleep(500);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Error: Sleeping thread has been interupted. Canceling animation.\n");
+                return;
+            }
             System.out.print(text + "\r");
         }
         String text = end;
